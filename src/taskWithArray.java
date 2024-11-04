9692a5bdd03d798f41b9afb890749bc2fe391972
MaxKhlystov <maksim3848@mail.ru>
2024-11-04 19:45:18 +0800

    Update taskWithArray.java

--- src/taskWithArray.java
+++ src/taskWithArray.java
@@ -1,16 +1,1 @@
-public class taskWithArray {
-    int size_array;
-    public taskWithArray(int size_array){
-        this.size_array=size_array;
-    }
-    int [] FibbonachiNumbersArray = new int[size_array];
-    void write(){
-        System.out.println(size_array + " первых чисел Фиббоначи: ");
-        for (int i = 0; i < size_array; i++) {
-        if (i < 2) {
-            FibbonachiNumbersArray[i] = 1;
-        } else FibbonachiNumbersArray[i] = FibbonachiNumbersArray[i - 1] + FibbonachiNumbersArray[i - 2];
-        System.out.println(FibbonachiNumbersArray[i]);
-        }
-    }
-}
+
